3c1156e1bdc6ad6e342a0b2fb5f0fe15282ca083
lishu gupta <lishugupta652@gmail.com>
2019-09-12 00:47:19 +0530

    stated Creating awesome content

---
+++ src/Components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer>
+            <h2>Heya Follow <i class="fa fa-angle-down" aria-hidden="true"></i></h2>
+        </footer>
+    )
+}
+
+export default Footer
---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div>
+            <header>
+                <h1>Scribbling Pens</h1>
+            </header>
+        </div>
+    )
+}
+
+export default Header
